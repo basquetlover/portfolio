46e6c02b7d1e5ec60df2ed96ca9ff4bd967ecca1
basquetlover <85557755+basquetlover@users.noreply.github.com>
2025-07-16 20:38:06 +0200

    Primer commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+// @ts-ignore
 
+import tailwindcss from "@tailwindcss/vite";
+
+import vercel from '@astrojs/vercel';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+ enabled: false
+},
+ adapter: vercel(),
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,svelte,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        azul: '#192253',
+        magenta: '#c23a97',
+        rosa: '#fb8ad7',
+        blanco: '#ffffff'
+      },
+    },
+  },
+  plugins: [],
+}
+
